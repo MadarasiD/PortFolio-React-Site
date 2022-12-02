ff28d462fca6918d45d311386aa82d62defbf22f
MadarasiD <madarka0913@gmail.com>
2022-12-02 12:03:36 +0100

    update

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import FacebookIcon from '@mui/icons-material/Facebook';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import  cssBattle    from '../Home/cssb.svg'
+
+
 
 export default function Profile() {
   return (
@@ -9,19 +15,19 @@ export default function Profile() {
                 <div className="colz">
 
                     <a href="#">
-                    <FacebookIcon />
+                        <FontAwesomeIcon icon={faFacebook} />
                     </a>
 
                     <a href="#">
-                        <i className='fa fa-google-plus-square>'></i>
+                        <FontAwesomeIcon icon={faLinkedin} />
                     </a>
 
                     <a href="#">
-                        <i className='fa fa-linkedin>'></i>
+                        <FontAwesomeIcon icon={faGithub} />
                     </a>
 
-                    <a href="#">
-                        <i className='fa fa-facebook-square>'></i>
+                    <a href="https://cssbattle.dev/player/p2yKgsM6Q4WIjoIJqYFVXT0D4O43" target="_blank">
+                    <img src={cssBattle} alt="" />
                     </a>
                 </div>
             </div>
