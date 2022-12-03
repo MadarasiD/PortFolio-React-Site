e36d535fb7766002a291878b9cd40d6669c83bcc
MadarasiD <madarka0913@gmail.com>
2022-12-03 22:19:20 +0100

    update

---
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { TOTAL_SCREENS, GET_SCREEN_INDEX } from '../../../utilities/commonUtils';
+import ScrollService from '../../../utilities/ScrollService';
+import { faBars } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Header.css'
+
+export default function Header() {
+
+    const [ selectedScreen, setSelectedScreen ] = useState(0)
+    const [ showHeaderOptions, setShowHeaderOptions ] = useState(false)
+
+    const
+
+  return (
+    <div>
+
+    </div>
+  )
+}
