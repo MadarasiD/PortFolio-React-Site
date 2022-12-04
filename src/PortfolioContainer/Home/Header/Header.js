f39e434f0cf3f6a9e70c834dcd96d13af52f660b
MadarasiD <madarka0913@gmail.com>
2022-12-04 19:55:23 +0100

    update

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { TOTAL_SCREENS, GET_SCREEN_INDEX } from '../../../utilities/commonUtils';
 import ScrollService from '../../../utilities/ScrollService';
-import { faBars } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Header.css'
 
@@ -62,6 +62,10 @@ export default function Header() {
 
                 <div className="header-logo">
                     <span>Madarasi Dániel</span>
+                </div>
+
+                <div className={(showHeaderOptions)? "header-option show-hamburger-options": "header-option"}>
+                    {getHeaderOptions()}
                 </div>
             </div>
 
--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../Home/Home.css'
 import Footer from '../Home/Footer/Footer'
-import Profile from './Profile/Profile'
+import Profile from './Profile/Profile';
+import Header from './Header/Header';
 
 export default function () {
   return (
     <div className='home-container'>
         
+        <Header />
         <Profile />
         <Footer />
 
