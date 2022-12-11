28bd97042fbe623576208255cc91442619fcbed9
MadarasiD <madarka0913@gmail.com>
2022-12-11 20:24:56 +0100

    update

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -4,6 +4,15 @@ import ScrollService from '../../utilities/ScrollService';
 import Animation from '../../utilities/Animation';
 
 export default function (props) {
+
+  let fadeInScreenHandler = (screen) => {
+
+    if(screen.fadeScreen !== props.id)
+    return
+    Animation.animation.fadeInScreen(props.id)
+
+  }
+
   return (
     <div className='aboutme-container screen-container'>
         <div className="about-me-parent">
