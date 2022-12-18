6c15eceb739cfcd7f90c247c3affce219811cb77
MadarasiD <madarka0913@gmail.com>
2022-12-18 19:00:09 +0100

    Resume style

--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -20,31 +20,28 @@ export default function Resume(props) {
     
 
     const ResumeHeading = (props) => {
-        <div className="resume-heading">
-            <div className="resume-main-heading">
-                <div className="heading-bullet">
-                    <span> {props.heading ? props.heading : ''} </span>
-                    {props.fromDate && props.toDate ? (
-                        <div className="heading-date">
-                            {props.fromDate + '_' + props.toDate}
-                        </div>
-                    ): (
-
-                        <div></div>
-
-                    )}
-                </div>
-
-                <div className="resume-sub-heading">
-                    <span> {props.subHeading ? props.subHeading : ''} </span>
-                </div>
-
-                <div className="resume-heading-description">
-                    <span> {props.description ? props.description : ''} </span>
-                </div>
-
+        return (
+            <div className="resume-heading">
+              <div className="resume-main-heading">
+                <div className="heading-bullet"></div>
+                <span>{props.heading ? props.heading : ""}</span>
+                {props.fromDate && props.toDate ? (
+                  <div className="heading-date">
+                    {props.fromDate + "-" + props.toDate}
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+              <div className="resume-sub-heading">
+                <span>{props.subHeading ? props.subHeading : ""}</span>
+              </div>
+              <div className="resume-heading-description">
+                <span>{props.description ? props.description : ""}</span>
+              </div>
             </div>
-        </div>
+          );
+      
     }
 
     const resumeBullets = [
