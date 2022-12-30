d0c565777aa558c4509f780a69d6fd0ed0d58dbb
MadarasiD <98647320+MadarasiD@users.noreply.github.com>
2022-12-30 13:22:23 +0100

    homeProjects update

--- src/PortfolioContainer/HomeProjects/HomeProjects.js
+++ src/PortfolioContainer/HomeProjects/HomeProjects.js
@@ -16,6 +16,29 @@ export default function HomeProjects(props) {
     
       const fadeInSubscription =
         ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+        const options = {
+
+            loop: true,
+            margin: 0,
+            animateIn: "bounceInRight",
+            animateOut: "bounceOutRight",
+            dots: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            responsive: {
+                0:{
+                    items: 1,  
+                },
+                768:{
+                    items: 1,  
+                },
+                1000:{
+                    items: 3,  
+                },
+            }
+
+        }
     
 
   return (
@@ -29,7 +52,8 @@ export default function HomeProjects(props) {
         <section className="projects-section" id={props.id || ''}>
             <div className="container">
                 <div className="row">
-                    <OwlCarousel className='owl-carousel' id='projects-carousel'>
+                    <OwlCarousel className='owl-carousel' id='projects-carousel' {...options}>
+
                         <div className="col-lg-12">
                             <div className="project-item">
                                 <div className="project-comment">
@@ -42,7 +66,42 @@ export default function HomeProjects(props) {
                                 <div className="client-info">
                                     <img src={require('../../assets/Projects/LoginForm.jpg')} alt="Login Form" />
                                     <a href="https://12-login-form.vercel.app/" target='_blank'>click to see</a>
-                                tell us something about your project
+                                    <h5>tell us something about your project</h5>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-12">
+                            <div className="project-item">
+                                <div className="project-comment">
+                                    <p>
+                                        <i className="fa fa-quote-left"/>
+                                        what is the name of the project
+                                        <i className="fa fa-quote-right"/>
+                                    </p>
+                                </div>
+                                <div className="client-info">
+                                    <img src={require('../../assets/Projects/LoginForm.jpg')} alt="Login Form" />
+                                    <a href="https://12-login-form.vercel.app/" target='_blank'>click to see</a>
+                                    <h5>tell us something about your project</h5>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="col-lg-12">
+                            <div className="project-item">
+                                <div className="project-comment">
+                                    <p>
+                                        <i className="fa fa-quote-left"/>
+                                        what is the name of the project
+                                        <i className="fa fa-quote-right"/>
+                                    </p>
+                                </div>
+                                <div className="client-info">
+                                    <img src={require('../../assets/Projects/LoginForm.jpg')} alt="Login Form" />
+                                    <a href="https://12-login-form.vercel.app/" target='_blank'>click to see</a>
+                                    <h5>tell us something about your project</h5>
                                 </div>
                             </div>
                         </div>
